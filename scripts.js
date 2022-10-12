73d20a2dda99f15ea9920269121910c8ca5eb2e1
Robert Siciliano <Robert.siciliano22@gmail.com>
2022-10-12 11:43:29 -0400

    Revamped Logic

--- scripts.js
+++ scripts.js
@@ -1,86 +1,93 @@
-// Initialize array of values that will be shown on the screen
-let displayArray = [];
- // Create array for current number 
- let currentValue = [];
+// Elements
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('.equals');
+const clearButton = document.querySelector('.clear');
+const historyDisplay = document.getElementById('historyDisplay');
+const currentDisplay = document.getElementById('currentDisplay');
 
-function add(num1, num2) {
-    value = num1 + num2;
-    return calcDisplay(value);
-};  
+// Might not need this
+const operators = ['*', '/', '+', '-'];
 
+// Clear everything initially
+let operator = "";
+let nextOperator = "";
+let num1 = "";
+let num2 = "";
+let answer = "";
+historyDisplay.innerHTML = "";
+currentDisplay.innerHTML = "";
 
-function subtract(num1, num2) {
-    value = num1 - num2;
-    return calcDisplay(value);
-};
+function operate(historyDisplayText, currentDisplayText) {
+    // Split historyDisplayText to separate the number and operator
+    historyDisplayText_split = historyDisplayText.split(" ");
 
+    num1 = Number(historyDisplayText_split[0]);
+    operator = historyDisplayText_split[1];
+    num2 = Number(currentDisplayText);
 
-function multiply(num1, num2) {
-    value = num1 * num2;
-    return calcDisplay(value);
-};
+    // Perform correct calculation
+    if(operator == "*") answer = num1 * num2;
+    if(operator == "/") answer =  num1 / num2;
+    if(operator == "+") answer =  num1 + num2;
+    if(operator == "-") {
+        if (num2 == 0) return alert("Cannot divide by zero!")
+        else answer = num1 - num2;
+    }
 
-
-function divide(num1, num2) {
-    value = num1 / num2;
-    return calcDisplay(value);
-};
-
-
-function operate(array) {
-    console.log(array);
-    for(let i = 0; i < array.length; i++) {
-        // If i is +,-,*, or /, set num1 & num2 to the values left and right of the operator
-        if (isNaN(array[i])) {
-            const num1 = parseInt(array[i - 1]);
-            const num2 = parseInt(array[i + 1]);
-
-            // Depending on the operator, send numbers to correct function
-        if(array[i] == "+") return add(num1, num2);
-        if(array[i] == "-") return subtract(num1, num2);
-        if(array[i] == "*") return multiply(num1, num2);
-        if(array[i] == "/") return divide(num1, num2);
-        };
-    };
-};
-
-// Function that takes a value as input and displays it on the calculator screen 
-function calcDisplay(value) {
-    
-    // If value is a number map it to currentValue array
-    if (!isNaN(value)) {
-        currentValue.push(parseInt(value));
+    if (nextOperator.length > 0) {
+        // Change existing displays
+        historyDisplay.innerHTML = `${answer} ${nextOperator}`;
+        currentDisplay.innerHTML = "";
+        nextOperator = "";
     }
     else {
-        // Append the value to the display array
-        let number = currentValue.join('');
-        displayArray.push(number, value);   
+        historyDisplay.innerHTML = "";
+        currentDisplay.innerHTML = answer;
     }
-    console.log(currentValue);
-    console.log(displayArray);
-    
-    
+};
 
-    // Get calc-screen input
-    let display = document.getElementById('display');
-    display.value = `current: ${value}, all: ${displayArray.join(' ')}`;
+// Function that displays previous inputs
+function displayHistory(operator) {
+    // If there is already an equation in historyDisplay send it to operate function
+    if (historyDisplay.innerHTML.length > 1) {
+        nextOperator = operator;
+        return operate(historyDisplay.innerHTML, currentDisplay.innerHTML);
+    }
+    else {
+        // Make history display the current display, then clear current display
+        historyDisplay.innerHTML = currentDisplay.innerHTML + " " + operator;
+        currentDisplay.innerHTML = "";
+    }
 }
 
-// Get all of the buttons into a variable
-let buttons = document.querySelectorAll('button');
-// For each button add click event listener that sends button value to calcDisplay function
-buttons.forEach(button => {
-    button.addEventListener('click', function(e) {
-        // Get value of the clicked button
-        let value = e.target.innerHTML;
-        // Handle divide and multiply symbols
-        if (value == "x") value = '*';
-        if (value == "÷") value = '/';
-        
-        // If the value is AC, send to a clear function
+// Function that displays current value
+function displayCurrent(current) {
+    // Only allow one decimal point
+    if (current == "." && currentDisplay.innerHTML.includes(".")) return;
+    currentDisplay.innerHTML += current;
+};
 
-        // If the value is = send it to the operate function
-        if (value == "=") return operate(displayArray);
-        calcDisplay(value);
+
+// Event listener for number buttons and decimal button
+numberButtons.forEach(numerButton => {
+    numerButton.addEventListener('click', function(e) {
+        let current = e.target.innerHTML;
+        displayCurrent(current);
     })
-})
+});
+// Event listener for operator buttons
+operatorButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', function(e) {
+        // Do nothing if operator is clicked before any numbers
+        if (currentDisplay.innerHTML.length < 1) return
+
+        operator = e.target.innerHTML;
+        if (operator == "x") operator = "*";
+        if (operator == "÷") operator = "/";
+        displayHistory(operator);
+    })
+});
+// Event listener for equals button
+equalsButton.addEventListener('click', () => operate(historyDisplay.innerHTML, currentDisplay.innerHTML));
+// Event listener for clear button
